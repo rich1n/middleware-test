289e77cca4d107a5c592aca4992ac251ef644c35
rich1n <ricard122@gmail.com>
2023-01-21 22:20:47 -0400

    middleware v2

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// The country to block from accessing the secret page
+const BLOCKED_COUNTRY = 'VE';
+
+// Trigger this middleware to run on the `/secret-page` route
+export const config = {
+  matcher: '/secret-page',
+};
+
+export function middleware(req: NextRequest) {
+  // Extract country. Default to US if not found.
+  const country = (req.geo && req.geo.country) || 'US';
+
+  console.log(`Visitor from ${country}`);
+
+  // Specify the correct route based on the requests location
+  if (country === BLOCKED_COUNTRY) {
+    req.nextUrl.pathname = '/login';
+  } else {
+    req.nextUrl.pathname = `/secret-page`;
+  }
+
+  // Rewrite to URL
+  return NextResponse.rewrite(req.nextUrl);
+}
---
+++ pages/secret-page.tsx
@@ -0,0 +1,9 @@
+export default function SecretPage() {
+    return (
+      <div>
+        <h1>Secret Page</h1>
+        <p>This is a secret page!</p>
+      </div>
+    );
+  }
+  
